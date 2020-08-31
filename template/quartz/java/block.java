5c9ca39da2ff98d63f48f96bb88a6a4676f0999c
David Montandon <dave.tendon@gmail.com>
2020-08-31 23:42:53 +0200

    slab and fix lang

--- template/quartz/java/block.java
+++ template/quartz/java/block.java
@@ -6,7 +6,7 @@
 	public static final RegistryObject<Block> CHISELED_%BLOCK_UPPER%_BLOCK = BLOCKS.register("chiseled_%BLOCK%_block",
 			() -> new Block(Block.Properties.create(Material.ROCK, MaterialColor.QUARTZ).hardnessAndResistance(0.8F)));
 
-	public static final RegistryObject<Block> %BLOCK_UPPER%_PILLAR = BLOCKS.register("%BLOCK%%_pillar",
+	public static final RegistryObject<Block> %BLOCK_UPPER%_PILLAR = BLOCKS.register("%BLOCK%_pillar",
 			() -> new RotatedPillarBlock(Block.Properties.create(Material.ROCK, MaterialColor.QUARTZ).hardnessAndResistance(0.8F)));
 
 	public static final RegistryObject<Block> %BLOCK_UPPER%_STAIRS = BLOCKS.register("%BLOCK%_stairs",
@@ -19,3 +19,9 @@
 	public static final RegistryObject<Block> SMOOTH_%BLOCK_UPPER%_STAIRS = BLOCKS.register("smooth_%BLOCK%_stairs",
 			() -> new StairsBlock(() -> BlockInit.SMOOTH_%BLOCK_UPPER%.get().getDefaultState(),
 					Block.Properties.create(Material.ROCK, MaterialColor.QUARTZ)));
+					
+	public static final RegistryObject<Block> %BLOCK_UPPER%_SLAB = BLOCKS.register("%BLOCK%_slab",
+			() -> new SlabBlock(Block.Properties.create(Material.ROCK, MaterialColor.QUARTZ).hardnessAndResistance(2.0F, 6.0F)));
+
+	public static final RegistryObject<Block> SMOOTH_%BLOCK_UPPER%_SLAB = BLOCKS.register("smooth_%BLOCK%_slab",
+			() -> new SlabBlock(Block.Properties.create(Material.ROCK, MaterialColor.QUARTZ).hardnessAndResistance(2.0F, 6.0F)));
